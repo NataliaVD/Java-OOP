4ea96d3b06cd2bae41eb946cc7ec6be516c3c68e
Natalia Dimchovska <nataliadimchovska@gmail.com>
2022-05-07 14:22:22 +0300

    Add files via upload

---
+++ UnitTesting/UnitTesting/Target.java
@@ -0,0 +1,12 @@
+package UnitTesting;
+
+public interface Target {
+
+    int getHealth();
+
+    void takeAttack(int attackPoints);
+
+    int giveExperience();
+
+    boolean isDead();
+}
---
+++ UnitTesting/UnitTesting/Weapon.java
@@ -0,0 +1,10 @@
+package UnitTesting;
+
+public interface Weapon {
+
+    int getDurabilityPoints();
+
+    void attack(Target target);
+
+    int getAttackPoints();
+}
